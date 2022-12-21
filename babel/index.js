0510dd2922ada0d9fc7b41fdc1cc402131071baf
jinweios <jinwei.peng@outlook.com>
2022-12-21 15:48:42 +0800

    feat: babel preset和corejs

--- babel/index.js
+++ babel/index.js
@@ -1,3 +1,5 @@
+import base from './base.js'
+console.log(base)
 const a = 1;
 const b = () => console.log('hello world')
 const c = {
@@ -10,3 +12,10 @@ const {name, age: peopelAge, info} = c;
 
 // const test = c && c.info && c.info.name
 const test = c?.info?.name?.age
+
+const array = [1, 2, 3, 4]
+array.forEach(item => console.log(item))
+
+const asyncTask = new Promise((resolve, reject) => {
+    resolve(1)
+})
